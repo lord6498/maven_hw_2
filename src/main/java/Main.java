e5d20e1022e7cd152e5e0e0111a07ca63ea3b570
Nick <>
2021-01-26 17:10:50 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,11 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long expected = 30;
+    long actual = service.calculate(amount, registered);
+    boolean passed = expected == actual;
+    System.out.print(passed);
+
   }
 }
 
