b445a08b8f3a87fd8100b255f0a4d486f613332d
Nick <>
2021-01-26 17:12:23 +0300

    reformat code done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
 
-    long expected = 30;
-    long actual = service.calculate(amount, registered);
-    boolean passed = expected == actual;
-    System.out.print(passed);
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        boolean passed = expected == actual;
+        System.out.print(passed);
 
-  }
+    }
 }
 
